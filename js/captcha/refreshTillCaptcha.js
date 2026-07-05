054e906ac1490e5c623c2161bfe5354a851a561a
chanu0073 <chanu6692@gmail.com>
2026-07-05 10:29:25 +0530

    fix: remove refresh for registration page

--- js/captcha/refreshTillCaptcha.js
+++ js/captcha/refreshTillCaptcha.js
@@ -1,7 +1,3 @@
-if (document.URL.includes("registration.vitap.ac.in")) {
-  return;
-}
-
 let captchaImg = document.querySelector("img.form-control")
 let homePageNavbar = document.getElementById("stdForm");
 if (captchaImg == null && homePageNavbar == null) {
